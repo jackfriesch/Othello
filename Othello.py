668cb778789cf3ae8db9ba340501ef684e9436de
J Friesch <frieschj@oregonstate.edu>
2023-06-02 16:31:44 -0500

    Test commit

--- Othello.py
+++ Othello.py
@@ -37,206 +37,206 @@ class Othello:
     def make_move(self, color, piece_position):
         if color.lower() == "white":
             move_made = piece_position
-            self._board[piece_position[0]][piece_position[1]] = "O "
+            self._board[piece_position[0]][piece_position[1]] = "O"
             return self.flip_up(color, piece_position, [], 0, move_made)
         if color.lower() == "black":
             move_made = piece_position
-            self._board[piece_position[0]][piece_position[1]] = "X "
+            self._board[piece_position[0]][piece_position[1]] = "X"
             return self.flip_up(color, piece_position, [], 0, move_made)
 
     def flip_up(self, color, piece_position, up_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0] - 1][piece_position[1]] == "X ":
+            if self._board[piece_position[0] - 1][piece_position[1]] == "X":
                 count += 1
                 up_list.append((piece_position[0] - 1, piece_position[1]))
                 return self.flip_up(color, (piece_position[0] - 1, piece_position[1]), up_list, count, move_made)
-            if self._board[piece_position[0] - 1][piece_position[1]] == "O " and count >= 1:
+            if self._board[piece_position[0] - 1][piece_position[1]] == "O" and count >= 1:
                 for coords in up_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
                 return self.flip_down(color, move_made, [], 0, move_made)
             else:
                 return self.flip_down(color, move_made, [], 0, move_made)
         if color.lower() == "black":
-            if self._board[piece_position[0] - 1][piece_position[1]] == "O ":
+            if self._board[piece_position[0] - 1][piece_position[1]] == "O":
                 count += 1
                 up_list.append((piece_position[0] - 1, piece_position[1]))
                 return self.flip_up(color, (piece_position[0] - 1, piece_position[1]), up_list, count, move_made)
-            if self._board[piece_position[0] - 1][piece_position[1]] == "X " and count >= 1:
+            if self._board[piece_position[0] - 1][piece_position[1]] == "X" and count >= 1:
                 for coords in up_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
                 return self.flip_down(color, move_made, [], 0, move_made)
             else:
                 return self.flip_down(color, move_made, [], 0, move_made)
 
     def flip_down(self, color, piece_position, down_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0] + 1][piece_position[1]] == "X ":
+            if self._board[piece_position[0] + 1][piece_position[1]] == "X":
                 count += 1
                 down_list.append((piece_position[0] + 1, piece_position[1]))
                 return self.flip_down(color, (piece_position[0] + 1, piece_position[1]), down_list, count, move_made)
-            if self._board[piece_position[0] + 1][piece_position[1]] == "O " and count >= 1:
+            if self._board[piece_position[0] + 1][piece_position[1]] == "O" and count >= 1:
                 for coords in down_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
                 return self.flip_right(color, move_made, [], 0, move_made)
             else:
                 return self.flip_right(color, move_made, [], 0, move_made)
         if color.lower() == "black":
-            if self._board[piece_position[0] + 1][piece_position[1]] == "O ":
+            if self._board[piece_position[0] + 1][piece_position[1]] == "O":
                 count += 1
                 down_list.append((piece_position[0] + 1, piece_position[1]))
                 return self.flip_down(color, (piece_position[0] + 1, piece_position[1]), down_list, count, move_made)
-            if self._board[piece_position[0] + 1][piece_position[1]] == "X " and count >= 1:
+            if self._board[piece_position[0] + 1][piece_position[1]] == "X" and count >= 1:
                 for coords in down_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
                 return self.flip_right(color, move_made, [], 0, move_made)
             else:
                 return self.flip_right(color, move_made, [], 0, move_made)
 
     def flip_right(self, color, piece_position, right_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0]][piece_position[1] + 1] == "X ":
+            if self._board[piece_position[0]][piece_position[1] + 1] == "X":
                 count += 1
                 right_list.append((piece_position[0], piece_position[1] + 1))
                 return self.flip_right(color, (piece_position[0], piece_position[1] + 1), right_list, count, move_made)
-            if self._board[piece_position[0]][piece_position[1] + 1] == "O " and count >= 1:
+            if self._board[piece_position[0]][piece_position[1] + 1] == "O" and count >= 1:
                 for coords in right_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
                 return self.flip_left(color, move_made, [], 0, move_made)
             else:
                 return self.flip_left(color, move_made, [], 0, move_made)
         if color.lower() == "black":
-            if self._board[piece_position[0]][piece_position[1] + 1] == "O ":
+            if self._board[piece_position[0]][piece_position[1] + 1] == "O":
                 count += 1
                 right_list.append((piece_position[0], piece_position[1] + 1))
                 return self.flip_right(color, (piece_position[0], piece_position[1] + 1), right_list, count, move_made)
-            if self._board[piece_position[0]][piece_position[1] + 1] == "X " and count >= 1:
+            if self._board[piece_position[0]][piece_position[1] + 1] == "X" and count >= 1:
                 for coords in right_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
                 return self.flip_left(color, move_made, [], 0, move_made)
             else:
                 return self.flip_left(color, move_made, [], 0, move_made)
 
     def flip_left(self, color, piece_position, left_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0]][piece_position[1] - 1] == "X ":
+            if self._board[piece_position[0]][piece_position[1] - 1] == "X":
                 count += 1
                 left_list.append((piece_position[0], piece_position[1] - 1))
                 return self.flip_left(color, (piece_position[0], piece_position[1] - 1), left_list, count, move_made)
-            if self._board[piece_position[0]][piece_position[1] - 1] == "O " and count >= 1:
+            if self._board[piece_position[0]][piece_position[1] - 1] == "O" and count >= 1:
                 for coords in left_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
                 return self.flip_upleft(color, move_made, [], 0, move_made)
             else:
                 return self.flip_upleft(color, move_made, [], 0, move_made)
         if color.lower() == "black":
-            if self._board[piece_position[0]][piece_position[1] - 1] == "O ":
+            if self._board[piece_position[0]][piece_position[1] - 1] == "O":
                 count += 1
                 left_list.append((piece_position[0], piece_position[1] - 1))
                 return self.flip_left(color, (piece_position[0], piece_position[1] - 1), left_list, count, move_made)
-            if self._board[piece_position[0]][piece_position[1] - 1] == "X " and count >= 1:
+            if self._board[piece_position[0]][piece_position[1] - 1] == "X" and count >= 1:
                 for coords in left_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
                 return self.flip_upleft(color, move_made, [], 0, move_made)
             else:
                 return self.flip_upleft(color, move_made, [], 0, move_made)
 
     def flip_upleft(self, color, piece_position, upleft_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "X ":
+            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "X":
                 count += 1
                 upleft_list.append((piece_position[0] - 1, piece_position[1] - 1))
                 return self.flip_upleft(color, (piece_position[0] - 1, piece_position[1] - 1), upleft_list, count,
                                         move_made)
-            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "O " and count >= 1:
+            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "O" and count >= 1:
                 for coords in upleft_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
                 return self.flip_upright(color, move_made, [], 0, move_made)
             else:
                 return self.flip_upright(color, move_made, [], 0, move_made)
         if color.lower() == "black":
-            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "O ":
+            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "O":
                 count += 1
                 upleft_list.append((piece_position[0] - 1, piece_position[1] - 1))
                 return self.flip_upleft(color, (piece_position[0] - 1, piece_position[1] - 1), upleft_list, count,
                                         move_made)
-            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "X " and count >= 1:
+            if self._board[piece_position[0] - 1][piece_position[1] - 1] == "X" and count >= 1:
                 for coords in upleft_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
                 return self.flip_upright(color, move_made, [], 0, move_made)
             else:
                 return self.flip_upright(color, move_made, [], 0, move_made)
 
     def flip_upright(self, color, piece_position, upright_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "X ":
+            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "X":
                 count += 1
                 upright_list.append((piece_position[0] - 1, piece_position[1] + 1))
                 return self.flip_upright(color, (piece_position[0] - 1, piece_position[1] + 1), upright_list, count,
                                          move_made)
-            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "O " and count >= 1:
+            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "O" and count >= 1:
                 for coords in upright_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
                 return self.flip_downright(color, move_made, [], 0, move_made)
             else:
                 return self.flip_downright(color, move_made, [], 0, move_made)
         if color.lower() == "black":
-            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "O ":
+            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "O":
                 count += 1
                 upright_list.append((piece_position[0] - 1, piece_position[1] + 1))
                 return self.flip_upright(color, (piece_position[0] - 1, piece_position[1] + 1), upright_list, count,
                                          move_made)
-            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "X " and count >= 1:
+            if self._board[piece_position[0] - 1][piece_position[1] + 1] == "X" and count >= 1:
                 for coords in upright_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
                 return self.flip_downright(color, move_made, [], 0, move_made)
             else:
                 return self.flip_downright(color, move_made, [], 0, move_made)
 
     def flip_downright(self, color, piece_position, downright_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "X ":
+            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "X":
                 count += 1
                 downright_list.append((piece_position[0] + 1, piece_position[1] + 1))
                 return self.flip_downright(color, (piece_position[0] + 1, piece_position[1] + 1), downright_list, count,
                                            move_made)
-            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "O " and count >= 1:
+            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "O" and count >= 1:
                 for coords in downright_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
                 return self.flip_downleft(color, move_made, [], 0, move_made)
             else:
                 return self.flip_downleft(color, move_made, [], 0, move_made)
         if color.lower() == "black":
-            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "O ":
+            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "O":
                 count += 1
                 downright_list.append((piece_position[0] + 1, piece_position[1] + 1))
                 return self.flip_downright(color, (piece_position[0] + 1, piece_position[1] + 1), downright_list, count,
                                            move_made)
-            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "X " and count >= 1:
+            if self._board[piece_position[0] + 1][piece_position[1] + 1] == "X" and count >= 1:
                 for coords in downright_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
                 return self.flip_downleft(color, move_made, [], 0, move_made)
             else:
                 return self.flip_downleft(color, move_made, [], 0, move_made)
 
     def flip_downleft(self, color, piece_position, downleft_list, count, move_made):
         if color.lower() == "white":
-            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "X ":
+            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "X":
                 count += 1
                 downleft_list.append((piece_position[0] + 1, piece_position[1] - 1))
                 return self.flip_downleft(color, (piece_position[0] + 1, piece_position[1] - 1), downleft_list, count,
                                           move_made)
-            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "O " and count >= 1:
+            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "O" and count >= 1:
                 for coords in downleft_list:
-                    self._board[coords[0]][coords[1]] = "O "
+                    self._board[coords[0]][coords[1]] = "O"
         if color.lower() == "black":
-            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "O ":
+            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "O":
                 count += 1
                 downleft_list.append((piece_position[0] + 1, piece_position[1] - 1))
                 return self.flip_downleft(color, (piece_position[0] + 1, piece_position[1] - 1), downleft_list, count,
                                           move_made)
-            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "X " and count >= 1:
+            if self._board[piece_position[0] + 1][piece_position[1] - 1] == "X" and count >= 1:
                 for coords in downleft_list:
-                    self._board[coords[0]][coords[1]] = "X "
+                    self._board[coords[0]][coords[1]] = "X"
         return self._board
 
     def play_game(self, color, piece_position):
