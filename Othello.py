1f3f692f88b95716ac38da1e8094b7a601996813
J Friesch <frieschj@oregonstate.edu>
2023-06-08 14:13:49 -0500

    Final commit

--- Othello.py
+++ Othello.py
@@ -11,29 +11,38 @@ class Player:
         self._color = color
 
     def get_color(self):
+        """Returns the color of a Player object."""
         return self._color
 
     def get_name(self):
+        """Returns the name of a Player object"""
         return self._name
 
 
 class Othello:
+    """Represents a game of Othello. Initializes board, initializes each player's starting available moves, and
+    initializes the count of each piece to zero (to be calculated if game ends). Allows players to make moves,
+    flipping the appropriate pieces. Calculates each player's available moves after each turn. If a player makes a
+    move that ends the game, returns the winner and the number of pieces each player has on the final board. Allows
+    user to print the board at any point in the game."""
     def __init__(self):
         self._player_list = []
         self._board = [
-                        ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*"],
-                        ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
-                        ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
-                        ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
-                        ["*", ".", ".", ".", "O", "X", ".", ".", ".", "*"],
-                        ["*", ".", ".", ".", "X", "O", ".", ".", ".", "*"],
-                        ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
-                        ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
-                        ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
-                        ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*"]
-                    ]
+            ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*"],
+            ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
+            ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
+            ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
+            ["*", ".", ".", ".", "O", "X", ".", ".", ".", "*"],
+            ["*", ".", ".", ".", "X", "O", ".", ".", ".", "*"],
+            ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
+            ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
+            ["*", ".", ".", ".", ".", ".", ".", ".", ".", "*"],
+            ["*", "*", "*", "*", "*", "*", "*", "*", "*", "*"]
+        ]
         self._valid_w_moves = [(3, 5), (4, 6), (5, 3), (6, 4)]
         self._valid_b_moves = [(3, 4), (4, 3), (5, 6), (6, 5)]
+        self._white_pieces = 0
+        self._black_pieces = 0
 
     def create_player(self, player_name, color):
         """Creates a Player object and appends them to the list of players in the Othello class."""
@@ -42,6 +51,9 @@ class Othello:
     def play_game(self, color, piece_position):
         """Checks if a move is valid, if so, calls make_move to make the move. If not valid, returns 'Invalid move' and
         prints the available valid moves for that piece color."""
+        # return_winner() not called directly by play_game(). play_game() calls make_move(), which calls flip(),
+        # which calls recalc_valid_moves(), which calls return_winner() if the game is ended. So return_winner() is
+        # called if a player makes a winning move using play_game(), just not directly by this method.
         if color.lower() == "white":
             if piece_position in self._valid_w_moves:
                 return self.make_move(color, piece_position)
@@ -71,51 +83,46 @@ class Othello:
         to_flip = []
         directions = [(1, 0), (-1, 0), (0, 1), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
         if color.lower() == "white":
-            for x, y in directions:
+            for rshift, cshift in directions:
                 possible_flip = []
                 count = 0
-                for multiplier in range(1, 10):
-                    if 0 < row+x*multiplier < 9 and 0 < column+y*multiplier < 9:
-                        if self._board[row + x * multiplier][column + y * multiplier] == ".":
+                for multiplier in range(1, 9):
+                    if 0 < row + (rshift * multiplier) < 9 and 0 < column + (cshift * multiplier) < 9:
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == ".":
                             break
-                        if self._board[row+x*multiplier][column+y*multiplier] == "X":
-                            possible_flip.append((row+x*multiplier, column+y*multiplier))
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "X":
+                            possible_flip.append((row + (rshift * multiplier), column + (cshift * multiplier)))
                             count += 1
-                        if self._board[row+x*multiplier][column+y*multiplier] == "O":
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "O":
                             if count >= 1:
                                 for coords in possible_flip:
                                     to_flip.append(coords)
                                 break
                             else:
                                 break
-            return self.flip_pieces(color, to_flip)
         if color.lower() == "black":
-            for x, y in directions:
+            for rshift, cshift in directions:
                 possible_flip = []
                 count = 0
-                for multiplier in range(1, 10):
-                    if 0 < row + x * multiplier < 9 and 0 < column + y * multiplier < 9:
-                        if self._board[row + x * multiplier][column + y * multiplier] == ".":
+                for multiplier in range(1, 9):
+                    if 0 < row + (rshift * multiplier) < 9 and 0 < column + (cshift * multiplier) < 9:
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == ".":
                             break
-                        if self._board[row+x*multiplier][column+y*multiplier] == "O":
-                            possible_flip.append((row+x*multiplier, column+y*multiplier))
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "O":
+                            possible_flip.append((row + (rshift * multiplier), column + (cshift * multiplier)))
                             count += 1
-                        if self._board[row+x*multiplier][column+y*multiplier] == "X":
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "X":
                             if count >= 1:
                                 for coords in possible_flip:
                                     to_flip.append(coords)
                                 break
                             else:
                                 break
-            return self.flip_pieces(color, to_flip)
-
-    def flip_pieces(self, color, flip):
-        """Flips the pieces that were calculated in flip method to the appropriate color. Calls recalc_valid_moves."""
         if color.lower() == "white":
-            for coords in flip:
+            for coords in to_flip:
                 self._board[coords[0]][coords[1]] = "O"
         if color.lower() == "black":
-            for coords in flip:
+            for coords in to_flip:
                 self._board[coords[0]][coords[1]] = "X"
         return self.recalc_valid_moves()
 
@@ -139,58 +146,67 @@ class Othello:
         for piece in white_pieces:
             row = piece[0]
             column = piece[1]
-            for x, y in directions:
+            for rshift, cshift in directions:
                 count = 0
                 space_count = 0
-                for multiplier in range(1, 10):
-                    if 0 < row + x * multiplier < 9 and 0 < column + y * multiplier < 9:
-                        if self._board[row+x*multiplier][column+y*multiplier] == "X":
+                for multiplier in range(1, 9):
+                    if 0 < row + (rshift * multiplier) < 9 and 0 < column + (cshift * multiplier) < 9:
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "X":
                             count += 1
-                        if self._board[row+x*multiplier][column+y*multiplier] == ".":
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == ".":
                             if count >= 1:
-                                self._valid_w_moves.append((row + x * multiplier, column + y * multiplier))
+                                self._valid_w_moves.append((row + (rshift * multiplier),
+                                                            column + (cshift * multiplier)))
                                 space_count += 1
                             else:
                                 break
-                        if self._board[row+x*multiplier][column+y*multiplier] == "." and space_count >= 1:
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "." \
+                                and space_count >= 1:
                             break
-                        if self._board[row + x * multiplier][column + y * multiplier] == "O":
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "O":
                             break
         for piece in black_pieces:
             row = piece[0]
             column = piece[1]
-            for x, y in directions:
+            for rshift, cshift in directions:
                 count = 0
                 space_count = 0
-                for multiplier in range(1, 10):
-                    if 0 < row + x * multiplier < 9 and 0 < column + y * multiplier < 9:
-                        if self._board[row + x * multiplier][column + y * multiplier] == "O":
+                for multiplier in range(1, 9):
+                    if 0 < row + (rshift * multiplier) < 9 and 0 < column + (cshift * multiplier) < 9:
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "O":
                             count += 1
-                        if self._board[row + x * multiplier][column + y * multiplier] == ".":
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == ".":
                             if count >= 1:
-                                self._valid_b_moves.append((row + x * multiplier, column + y * multiplier))
+                                self._valid_b_moves.append((row + (rshift * multiplier),
+                                                            column + (cshift * multiplier)))
                                 space_count += 1
                             else:
                                 break
-                        if self._board[row+x*multiplier][column+y*multiplier] == "." and space_count >= 1:
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "." \
+                                and space_count >= 1:
                             break
-                        if self._board[row+x*multiplier][column+y*multiplier] == "X":
+                        if self._board[row + (rshift * multiplier)][column + (cshift * multiplier)] == "X":
                             break
         self._valid_w_moves = list(set(self._valid_w_moves))
         self._valid_b_moves = list(set(self._valid_b_moves))
         self._valid_w_moves.sort()
         self._valid_b_moves.sort()
         if len(self._valid_w_moves) == 0 and len(self._valid_b_moves) == 0:
+            for row in self._board:
+                for element in row:
+                    if element == "O":
+                        self._white_pieces += 1
+                    if element == "X":
+                        self._black_pieces += 1
+            print(f"Game is ended white piece: {self._white_pieces} black piece: {self._black_pieces}")
             return self.return_winner()
         return self._board
 
     def return_available_positions(self, color):
         """Returns the sorted list of the valid moves for the given color."""
         if color.lower() == "white":
-            self._valid_w_moves.sort()
             return self._valid_w_moves
         if color.lower() == "black":
-            self._valid_b_moves.sort()
             return self._valid_b_moves
 
     def print_board(self):
@@ -202,23 +218,15 @@ class Othello:
         """Once the game has ended (both player's valid move lists are empty), counts the number of each piece on the
         board. Whichever color has more pieces on the board wins. Returns 'Winner is white/black player: [name of
         white player/black player]'"""
-        white_count = 0
-        black_count = 0
         if self._player_list[0].get_color().lower() == "white":
             white_name = self._player_list[0].get_name()
             black_name = self._player_list[1].get_name()
         else:
             black_name = self._player_list[0].get_name()
             white_name = self._player_list[1].get_name()
-        for row in self._board:
-            for element in row:
-                if element == "O":
-                    white_count += 1
-                if element == "X":
-                    black_count += 1
-        if white_count > black_count:
+        if self._white_pieces > self._black_pieces:
             return "Winner is white player: " + white_name
-        if white_count < black_count:
+        if self._white_pieces < self._black_pieces:
             return "Winner is black player: " + black_name
-        if white_count == black_count:
-            return "It's a tie!"
+        if self._white_pieces == self._black_pieces:
+            return "It's a tie"
